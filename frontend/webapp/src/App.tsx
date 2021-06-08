af1ad6deab86c217459c4a5d4ab34d4ab8bd3dcf
ethan-heo <ethan.hb94@gmail.com>
2021-06-08 09:00:44 +0900

    chore: add emotion 설치 및 eslint 룰 설정

--- frontend/webapp/src/App.tsx
+++ frontend/webapp/src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
 import { Button } from "ui-components";
 
 function App() {
     return (
-        <div>
+        <div css={fontSize}>
             hello world
             <br /> <Button>hello ui components</Button>
         </div>
@@ -11,3 +12,7 @@ function App() {
 }
 
 export default App;
+
+const fontSize = css`
+    font-size: 2rem;
+`;
